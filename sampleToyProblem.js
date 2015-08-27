fcb0c2a2a45c46f3852241c74a5cbec557186b3f
Eric Kao <ericskao@gmail.com>
2015-08-27 10:14:42 -0700

    added ToyProblem

---
+++ sampleToyProblem.js
@@ -0,0 +1,25 @@
+/*
+Description:
+
+Write a function that takes an array/list of numbers and returns a number such that
+
+Explanation total([1,2,3,4,5]) => 48
+
+1+2=3--\ 3+5 =>     8 \
+2+3=5--/ \            ==  8+12=>20\
+          ==>5+7=> 12 / \           20+28 => 48
+3+4=7--\ /            == 12+16=>28/
+4+5=9--/ 7+9 =>     16  /
+ 
+if total([1,2,3]) => 8 then
+
+first+second => 3 \
+                   then 3+5 => 8
+second+third => 5 /
+
+*/
+
+var total = function (array) {
+  //write total here
+};
+
